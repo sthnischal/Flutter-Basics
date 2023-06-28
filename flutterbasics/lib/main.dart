afb7f2b4fb7f763c0257d2a7ab353a143fb3403b
Your Name <you@example.com>
2023-06-28 16:14:09 +0545

    ScrollView Example Added

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 20, 37, 229)),
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 4, 19, 186)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -46,45 +46,92 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text('Row and Column Example'),
+          title: Text('ScrollView Example'),
         ),
-        body:
-            // Container(
-            //   child: ElevatedButton(
-            //     child: Text('Click Me'),
-            //     onPressed: () {
-            //       print('Click on Button');
-            //     },
-            //   ),
-            // )
-
-            //Inkwell used for clickable into any widget
-            Center(
-                child: InkWell(
-                    onTap: () {
-                      print('Tapped on Container');
-                    },
-                    onLongPress: () {
-                      print('Long Pressed on Container');
-                    },
-                    onDoubleTap: () {
-                      print('Double Tapped on Container');
-                    },
-                    child: Container(
-                        width: 200,
-                        height: 200,
-                        color: Colors.amber,
-                        child: Center(
-                            child: InkWell(
-                                onTap: () {
-                                  print('Text Widget Tapped');
-                                },
-                                child: Text(
-                                  'Click Me',
-                                  style: TextStyle(
-                                    fontSize: 21,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                )))))));
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 11),
+                          height: 200,
+                          width: 200,
+                          color: Colors.blueGrey,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 11),
+                          height: 200,
+                          width: 200,
+                          color: Colors.lightGreen,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 11),
+                          height: 200,
+                          width: 200,
+                          color: Colors.black,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 11),
+                          height: 200,
+                          width: 200,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),
+                  height: 200,
+                  //width: 200,
+                  color: Colors.lightBlue,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),
+                  height: 200,
+                  //width: 200,
+                  color: Colors.yellowAccent,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),
+                  height: 200,
+                  //width: 200,
+                  color: Colors.orange,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),
+                  height: 200,
+                  //width: 200,
+                  color: Colors.lightGreen,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),
+                  height: 200,
+                  //width: 200,
+                  color: Colors.lightBlue,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),
+                  height: 200,
+                  //width: 200,
+                  color: Colors.yellowAccent,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),
+                  height: 200,
+                  //width: 200,
+                  color: Colors.orange,
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
