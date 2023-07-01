984aefb51d149fb18a3913cd2acba77843156a04
sthnischal <shresthanischal123@gmail.com>
2023-07-01 16:08:58 +0545

    Expanded widget on row and column

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -23,31 +23,81 @@ class DashBoardScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text('Dashboard'),
         ),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.blue.shade50,
-          child: Center(
-            child: Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  //borderRadius: BorderRadius.circular(11),
-                  //borderRadius: BorderRadius.only(topLeft: Radius.circular(21),bottomRight: Radius.circular(21))),
-                  //borderRadius: BorderRadius.all(Radius.circular(21)),
+        body:
+            //Row(
+            //   //mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     Expanded(
+            //       flex: 2, //for ratio
+            //       child: Container(
+            //         width: 50,
+            //         height: 100,
+            //         color: Colors.blue,
+            //       ),
+            //     ),
+            //     Expanded(
+            //       flex: 4,
+            //       child: Container(
+            //         width: 50,
+            //         height: 100,
+            //         color: Colors.orange,
+            //       ),
+            //     ),
+            //     Expanded(
+            //       flex: 2,
+            //       child: Container(
+            //         width: 50,
+            //         height: 100,
+            //         color: Colors.blueGrey,
+            //       ),
+            //     ),
+            //     Expanded(
+            //       flex: 1,
+            //       child: Container(
+            //         width: 50,
+            //         height: 100,
+            //         color: Colors.green,
+            //       ),
+            //     )
+            //   ],
+            // ),
 
-                  border: Border.all(
-                    width: 2,
-                    color: Colors.red,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 11, color: Colors.grey, spreadRadius: 6),
-                  ],
-                  shape: BoxShape.circle),
+            Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 2, //for ratio
+              child: Container(
+                //width: 50,
+                height: 100,
+                color: Colors.blue,
+              ),
             ),
-          ),
+            Expanded(
+              flex: 4,
+              child: Container(
+                //width: 50,
+                height: 100,
+                color: Colors.orange,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                //width: 50,
+                height: 100,
+                color: Colors.blueGrey,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                //width: 50,
+                height: 100,
+                color: Colors.green,
+              ),
+            )
+          ],
         ));
   }
 }
