1a9327fa99cabb72cf5e25ec8beda77049d36f68
sthnischal <shresthanischal123@gmail.com>
2023-07-01 15:48:57 +0545

    Decoration of Container Widget with boxdecoration

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -20,12 +20,34 @@ class DashBoardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
-      body: Container(
-        color: Colors.blue.shade50,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.blue.shade50,
+          child: Center(
+            child: Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  //borderRadius: BorderRadius.circular(11),
+                  //borderRadius: BorderRadius.only(topLeft: Radius.circular(21),bottomRight: Radius.circular(21))),
+                  //borderRadius: BorderRadius.all(Radius.circular(21)),
+
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.red,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 11, color: Colors.grey, spreadRadius: 6),
+                  ],
+                  shape: BoxShape.circle),
+            ),
+          ),
+        ));
   }
 }
