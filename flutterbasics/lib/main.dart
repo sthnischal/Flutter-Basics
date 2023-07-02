35df8c963cb74039993445ead16a8d8847978e6a
sthnischal <shresthanischal123@gmail.com>
2023-07-02 22:21:20 +0545

    style and Theme added

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterbasics/ui_helper/util.dart';
 
 void main() {
   runApp(FlutterApp());
@@ -10,7 +11,22 @@ class FlutterApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "FlutterApp",
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+          primarySwatch: Colors.green,
+          textTheme: TextTheme(
+            headline1: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+            headline2: TextStyle(
+                fontSize: 21, fontWeight: FontWeight.bold, color: Colors.green),
+            subtitle1: TextStyle(
+                fontSize: 11,
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic),
+            subtitle2: TextStyle(
+                fontSize: 11,
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic,
+                color: Colors.orange),
+          )),
       home: DashBoardScreen(),
     );
   }
@@ -22,18 +38,34 @@ class DashBoardScreen extends StatelessWidget {
     var arrNames = ['Ram', 'Shyam', 'Hari', 'Gopal', 'Sita', 'Sita'];
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Hello'),
-      ),
-      body: Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-              fontFamily: 'SquarePeg', //using custom font SquarePeg
-              fontSize: 55,
-              fontWeight: FontWeight.w500),
+        appBar: AppBar(
+          title: Text('Hello'),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            // Text(
+            //   'Text 1',
+            //   style: Theme.of(context).textTheme.headline1,
+            // ),
+            Text(
+              'Text 1',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline1!
+                  .copyWith(color: Colors.orange),
+            ),
+            Text(
+              'Text 2',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Text('Text 3', style: Theme.of(context).textTheme.headline2),
+            Text(
+              'Text 4',
+              //style: Theme.of(context).textTheme.subtitle2
+              //style: mTextStyle21(), //loading from util.dart file
+              style: mTextStyle11(textColor: Colors.blue),
+            ),
+          ],
+        ));
   }
 }
