5ac45657bea8cddf6a5914892854024dead59c48
sthnischal <shresthanischal123@gmail.com>
2023-07-03 08:02:46 +0545

    User Input from TextField Widget

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -20,26 +20,80 @@ class FlutterApp extends StatelessWidget {
 }
 
 class DashBoardScreen extends StatelessWidget {
+  var emailText = TextEditingController();
+  var passText = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    var arrNames = ['Ram', 'Shyam', 'Hari', 'Gopal', 'Sita', 'Sita'];
-
     return Scaffold(
         appBar: AppBar(
           title: Text('Hello'),
         ),
         body: Center(
-          child: Card(
-            shadowColor: Colors.green,
-            elevation: 12,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Hello World',
-                style: TextStyle(fontSize: 45),
-              ),
-            ),
-          ),
-        ));
+            child: Container(
+                width: 300,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      keyboardType: TextInputType.phone,
+                      //enabled: false,
+                      controller: emailText,
+                      decoration: InputDecoration(
+                          hintText: 'Enter Email Here',
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                              borderSide: BorderSide(
+                                color: Colors.deepOrange,
+                                width: 2,
+                              )),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                              borderSide: BorderSide(
+                                  color: Colors.blueAccent, width: 2)),
+                          disabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                              borderSide:
+                                  BorderSide(color: Colors.black12, width: 2)),
+                          //suffixText: "Username Exist",
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              Icons.remove_red_eye,
+                              color: Colors.orange,
+                            ),
+                            onPressed: () {},
+                            // border: OutlineInputBorder(
+                            //     borderRadius: BorderRadius.circular(11),
+                            //     borderSide:
+                            //         BorderSide(color: Colors.deepOrange))
+                          ),
+                          prefixIcon: Icon(
+                            //Icons.email,
+                            Icons.phone,
+                            color: Colors.orange,
+                          )),
+                    ),
+                    Container(height: 11),
+                    TextField(
+                      controller: passText,
+                      obscureText: true, //hidden text
+                      obscuringCharacter: '*',
+                      decoration: InputDecoration(
+                          hintText: 'Enter Password Here',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                              borderSide:
+                                  BorderSide(color: Colors.deepOrange))),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          String uEmail = emailText.text.toString();
+                          String uPass = passText.text;
+
+                          print('Email : $uEmail, Pass: $uPass');
+                        },
+                        child: Text('Login'))
+                  ],
+                ))));
   }
 }
