58c0a53c5823d2cc33d0b9ac87bc57d6aa3217f5
sthnischal <shresthanischal123@gmail.com>
2023-07-03 07:20:08 +0545

    Card Widget added

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -12,21 +12,8 @@ class FlutterApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "FlutterApp",
       theme: ThemeData(
-          primarySwatch: Colors.green,
-          textTheme: TextTheme(
-            headline1: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
-            headline2: TextStyle(
-                fontSize: 21, fontWeight: FontWeight.bold, color: Colors.green),
-            subtitle1: TextStyle(
-                fontSize: 11,
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.italic),
-            subtitle2: TextStyle(
-                fontSize: 11,
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.italic,
-                color: Colors.orange),
-          )),
+        primarySwatch: Colors.green,
+      ),
       home: DashBoardScreen(),
     );
   }
@@ -41,31 +28,18 @@ class DashBoardScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text('Hello'),
         ),
-        body: Column(
-          children: [
-            // Text(
-            //   'Text 1',
-            //   style: Theme.of(context).textTheme.headline1,
-            // ),
-            Text(
-              'Text 1',
-              style: Theme.of(context)
-                  .textTheme
-                  .headline1!
-                  .copyWith(color: Colors.orange),
+        body: Center(
+          child: Card(
+            shadowColor: Colors.green,
+            elevation: 12,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Hello World',
+                style: TextStyle(fontSize: 45),
+              ),
             ),
-            Text(
-              'Text 2',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            Text('Text 3', style: Theme.of(context).textTheme.headline2),
-            Text(
-              'Text 4',
-              //style: Theme.of(context).textTheme.subtitle2
-              //style: mTextStyle21(), //loading from util.dart file
-              style: mTextStyle11(textColor: Colors.blue),
-            ),
-          ],
+          ),
         ));
   }
 }
