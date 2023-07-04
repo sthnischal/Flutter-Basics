0bc9f9a9a5c84f55ed6677bb38c984da5607b465
sthnischal <shresthanischal123@gmail.com>
2023-07-04 07:58:26 +0545

    GridView and GridViewBuilder Widget added

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -21,51 +21,82 @@ class FlutterApp extends StatelessWidget {
 }
 
 class DashBoardScreen extends StatelessWidget {
-  var emailText = TextEditingController();
-  var passText = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
-    var time = DateTime.now();
+    var arrColors = [
+      Colors.red,
+      Colors.blue,
+      Colors.black,
+      Colors.green,
+      Colors.brown,
+      Colors.orange,
+      Colors.purpleAccent
+    ];
+
     return Scaffold(
         appBar: AppBar(
           title: Text('Hello'),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                'Select Date',
-                style: TextStyle(fontSize: 25),
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    DateTime? datePicked = await showDatePicker(
-                        context: context, // ? for if null value
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(2023),
-                        lastDate: DateTime(2025));
-
-                    if (datePicked != null) {
-                      print(
-                          'Date Selected: ${datePicked.day}-${datePicked.month}-${datePicked.year}');
-                    }
-                  },
-                  child: Text('Show')),
-              ElevatedButton(
-                  onPressed: () async {
-                    TimeOfDay? pickedTime = await showTimePicker(
-                        context: context, initialTime: TimeOfDay.now());
-                    //initialEntryMode: TimePickerEntryMode.input);
-                    //initialEntryMode: TimePickerEntryMode.dial); //it is bydefault date picker
-                    if (pickedTime != null) {
-                      print(
-                          'Time Selected: ${pickedTime.hour}: ${pickedTime.minute}');
-                    }
-                  },
-                  child: Text('Select Time'))
-            ],
+        body:
+            //gridviewbuilder directly fetch container with single line of code
+            GridView.builder(
+          itemBuilder: (context, index) {
+            return Container(
+              color: arrColors[index],
+            );
+          },
+          itemCount: arrColors.length,
+          gridDelegate:
+              //SliverGridDelegateWithFixedCrossAxisCount(
+              SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100,
+            //crossAxisCount: 3,
+            mainAxisSpacing: 11,
+            crossAxisSpacing: 11,
           ),
-        ));
+        )
+
+        // Column(
+        //   children: [
+        //     Container(
+        //       height: 200,
+        //       child: GridView.count(
+        //         crossAxisCount: 5,
+        //         crossAxisSpacing: 11,
+        //         mainAxisSpacing: 11,
+        //         children: [
+        //           Container(color: arrColors[0]),
+        //           Container(color: arrColors[1]),
+        //           Container(color: arrColors[2]),
+        //           Container(color: arrColors[3]),
+        //           Container(color: arrColors[4]),
+        //           Container(color: arrColors[5]),
+        //           Container(color: arrColors[6]),
+        //         ],
+        //       ),
+        //     ),
+        //     Container(
+        //       height: 100,
+        //     ),
+        //     Container(
+        //       height: 200,
+        //       child: GridView.extent(
+        //         maxCrossAxisExtent: 50,
+        //         crossAxisSpacing: 11,
+        //         mainAxisSpacing: 11,
+        //         children: [
+        //           Container(color: arrColors[0]),
+        //           Container(color: arrColors[1]),
+        //           Container(color: arrColors[2]),
+        //           Container(color: arrColors[3]),
+        //           Container(color: arrColors[4]),
+        //           Container(color: arrColors[5]),
+        //           Container(color: arrColors[6]),
+        //         ],
+        //       ),
+        //     )
+        //   ],
+        // )
+        );
   }
 }
