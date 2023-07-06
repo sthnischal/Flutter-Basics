487ff934129cffdbb58c87d6d0f241f4cb4e882c
sthnischal <shresthanischal123@gmail.com>
2023-07-06 06:53:20 +0545

    Stack Widget added

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -35,135 +35,34 @@ class DashBoardScreen extends StatelessWidget {
 
     return Scaffold(
         appBar: AppBar(
-          title: Text('Custom Widget'),
+          title: Text('Hello'),
         ),
         body: Container(
-          child: Column(children: [
-            catItems(),
-            Contact(),
-            SubCatItems(),
-            BottomMenu(),
-          ]),
-        ));
-  }
-}
-
-class catItems extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-        flex: 2,
-        child: Container(
-          color: Colors.green,
-          child: ListView.builder(
-              itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.all(11.0),
-                    child: SizedBox(
-                      width: 100,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.blue,
-                      ),
-                    ),
-                  ),
-              itemCount: 10,
-              scrollDirection: Axis.horizontal),
-        ));
-  }
-}
-
-class Contact extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 4,
-      child: Container(
-        color: Colors.yellow,
-        child: ListView.builder(
-            itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.blue,
-                    ),
-                    title: Text('Name'),
-                    subtitle: Text('Mob No.'),
-                    trailing: Icon(Icons.delete),
-                  ),
-                )),
-      ),
-    );
-  }
-}
-
-class SubCatItems extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: Container(
-        color: Colors.brown,
-        child: ListView.builder(
-          itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: 200,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(11), color: Colors.green),
-            ),
+          width: 300,
+          height: 300,
+          child: Stack(
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.blueGrey,
+              ),
+              // Container(
+              //   width: 160,
+              //   height: 160,
+              //   color: Colors.grey,
+              // ),
+              Positioned(
+                left: 21,
+                top: 21,
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.grey,
+                ),
+              )
+            ],
           ),
-          itemCount: 10,
-          scrollDirection: Axis.horizontal,
-        ),
-      ),
-    );
-  }
-}
-
-class BottomMenu extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 2,
-      child: Container(
-        color: Colors.blue,
-        child: GridView.count(
-          crossAxisCount: 4,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(11),
-                    color: Colors.green),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(11),
-                    color: Colors.green),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(11),
-                    color: Colors.green),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(11),
-                    color: Colors.green),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+        ));
   }
 }
