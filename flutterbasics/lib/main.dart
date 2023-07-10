a24f2c729963c88787f1086a980f9eaf1a33438a
sthnischal <shresthanischal123@gmail.com>
2023-07-10 16:23:18 +0545

    Constraint Box Widget Added

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -36,97 +38,26 @@ class DashBoardState extends State<DashBoardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Basic Calculation'),
+          title: Text('Hello'),
         ),
-        body: Container(
-          color: Colors.blue.shade100,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    controller: no1Controller,
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    controller: no2Controller,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                            onPressed: () {
-                              var no1 =
-                                  int.parse(no1Controller.text.toString());
-                              var no2 =
-                                  int.parse(no2Controller.text.toString());
-
-                              var sum = no1 + no2;
-
-                              result = 'The Sum of $no1 and $no2 is $sum';
-                              setState(() {});
-                            },
-                            child: Text('Add')),
-                        ElevatedButton(
-                            onPressed: () {
-                              var no1 =
-                                  int.parse(no1Controller.text.toString());
-                              var no2 =
-                                  int.parse(no2Controller.text.toString());
-
-                              var diff = no1 - no2;
-
-                              result =
-                                  'The difference between $no1  and $no2 is $diff';
-                              setState(() {});
-                            },
-                            child: Text('Sub')),
-                        ElevatedButton(
-                            onPressed: () {
-                              var no1 =
-                                  int.parse(no1Controller.text.toString());
-                              var no2 =
-                                  int.parse(no2Controller.text.toString());
-
-                              var product = no1 * no2;
-
-                              result =
-                                  'The product of $no1  and $no2 is $product';
-                              setState(() {});
-                            },
-                            child: Text('Mul')),
-                        ElevatedButton(
-                            onPressed: () {
-                              var no1 =
-                                  int.parse(no1Controller.text.toString());
-                              var no2 =
-                                  int.parse(no2Controller.text.toString());
-
-                              var times = no1 / no2;
-
-                              result =
-                                  'The $no1 can be divided by $no2, ${times.toStringAsFixed(2)} times.';
-                              setState(() {});
-                            },
-                            child: Text('Div')),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(21),
-                    child: Text(
-                      '$result',
-                      style: TextStyle(fontSize: 25, color: Colors.white),
-                    ),
-                  )
-                ],
-              ),
-            ),
+        body: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: 300,
+            minWidth: 200,
+            maxHeight: 300,
+            minHeight: 100,
+            //   maxWidth: 200,
+            //   maxHeight: 100,
+            // ),
+            // child: Text(
+            //   'Hello World Hello World Hello World Hello World Hello World Hello World Hello World',
+            //   style: TextStyle(fontSize: 21, overflow: TextOverflow.fade),
+            // ),
+          ),
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(
+                'Click Me Click Me Click Me Click Me Click Me Click Me Click Me'),
           ),
         ));
   }
