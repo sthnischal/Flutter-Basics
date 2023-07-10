6e280f1b21474178a2f96080995ea11419f57cb2
sthnischal <shresthanischal123@gmail.com>
2023-07-10 16:02:53 +0545

    Stateful Widget example with basic arithmatic operation

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -27,35 +27,107 @@ class DashBoardScreen extends StatefulWidget {
 }
 
 class DashBoardState extends State<DashBoardScreen> {
-  var count = 0;
+  var no1Controller = TextEditingController();
+  var no2Controller = TextEditingController();
+
+  var result;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Stateful'),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Count : $count',
-            style: TextStyle(fontSize: 34),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                //count++;
-                //count = count +1;
-                //count +=1;
+        appBar: AppBar(
+          title: Text('Basic Calculation'),
+        ),
+        body: Container(
+          color: Colors.blue.shade100,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    controller: no1Controller,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    controller: no2Controller,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              var no1 =
+                                  int.parse(no1Controller.text.toString());
+                              var no2 =
+                                  int.parse(no2Controller.text.toString());
 
-                setState(() {
-                  count++;
-                  print(count);
-                });
-              },
-              child: Text('Incremt Count'))
-        ],
-      )),
-    );
+                              var sum = no1 + no2;
+
+                              result = 'The Sum of $no1 and $no2 is $sum';
+                              setState(() {});
+                            },
+                            child: Text('Add')),
+                        ElevatedButton(
+                            onPressed: () {
+                              var no1 =
+                                  int.parse(no1Controller.text.toString());
+                              var no2 =
+                                  int.parse(no2Controller.text.toString());
+
+                              var diff = no1 - no2;
+
+                              result =
+                                  'The difference between $no1  and $no2 is $diff';
+                              setState(() {});
+                            },
+                            child: Text('Sub')),
+                        ElevatedButton(
+                            onPressed: () {
+                              var no1 =
+                                  int.parse(no1Controller.text.toString());
+                              var no2 =
+                                  int.parse(no2Controller.text.toString());
+
+                              var product = no1 * no2;
+
+                              result =
+                                  'The product of $no1  and $no2 is $product';
+                              setState(() {});
+                            },
+                            child: Text('Mul')),
+                        ElevatedButton(
+                            onPressed: () {
+                              var no1 =
+                                  int.parse(no1Controller.text.toString());
+                              var no2 =
+                                  int.parse(no2Controller.text.toString());
+
+                              var times = no1 / no2;
+
+                              result =
+                                  'The $no1 can be divided by $no2, ${times.toStringAsFixed(2)} times.';
+                              setState(() {});
+                            },
+                            child: Text('Div')),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(21),
+                    child: Text(
+                      '$result',
+                      style: TextStyle(fontSize: 25, color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
