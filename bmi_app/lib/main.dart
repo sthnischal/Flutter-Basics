75fb9e2e32ff718b1848b17b94be0b9fd4aae053
sthnischal <shresthanischal123@gmail.com>
2023-07-12 08:20:16 +0545

    logic and condition are added on bmi app

--- bmi_app/lib/main.dart
+++ bmi_app/lib/main.dart
@@ -33,58 +33,111 @@ class _MyHomePageState extends State<MyHomePage> {
   var ftController = TextEditingController();
   var inController = TextEditingController();
 
+  var result = "";
+  var bgColor = Colors.indigo.shade100;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('BMI'),
         ),
-        body: Center(
-          child: Container(
-            width: 300,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'BMI',
-                  style: TextStyle(fontSize: 34, fontWeight: FontWeight.w700),
-                ),
-                TextField(
-                  controller: wtController,
-                  decoration: InputDecoration(
-                      label: Text('Enter Your Weight (in Kgs)'),
-                      prefixIcon: Icon(Icons.line_weight)),
-                  keyboardType: TextInputType.number,
-                ),
-                SizedBox(height: 11),
-                TextField(
-                  controller: ftController,
-                  decoration: InputDecoration(
-                    label: Text('Enter your Height (in feet)'),
-                    prefixIcon: Icon(Icons.height),
+        body: Container(
+          color: bgColor,
+          child: Center(
+            child: Container(
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'BMI',
+                    style: TextStyle(fontSize: 34, fontWeight: FontWeight.w700),
                   ),
-                  keyboardType: TextInputType.number,
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                TextField(
-                  controller: inController,
-                  decoration: InputDecoration(
-                    label: Text('Enter Your Height (in inch)'),
-                    prefixIcon: Icon(Icons.height),
+                  TextField(
+                    controller: wtController,
+                    decoration: InputDecoration(
+                        label: Text('Enter Your Weight (in Kgs)'),
+                        prefixIcon: Icon(Icons.line_weight)),
+                    keyboardType: TextInputType.number,
                   ),
-                  keyboardType: TextInputType.number,
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                ElevatedButton(onPressed: () {}, child: Text('Calculate')),
-                SizedBox(
-                  height: 16,
-                ),
-                Text('data'),
-              ],
+                  SizedBox(height: 11),
+                  TextField(
+                    controller: ftController,
+                    decoration: InputDecoration(
+                      label: Text('Enter your Height (in feet)'),
+                      prefixIcon: Icon(Icons.height),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 11,
+                  ),
+                  TextField(
+                    controller: inController,
+                    decoration: InputDecoration(
+                      label: Text('Enter Your Height (in inch)'),
+                      prefixIcon: Icon(Icons.height),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        var wt = wtController.text.toString();
+                        var ft = ftController.text.toString();
+                        var inch = inController.text.toString();
+
+                        //check blank input
+
+                        if (wt != "" && ft != "" && inch != "") {
+                          //bmi calculation
+                          // convert input into integer
+                          var iWt = int.parse(wt);
+                          var iFt = int.parse(ft);
+                          var iInch = int.parse(inch);
+
+                          // calculate all value into kg/m for SI unit of BMI
+                          var tInch = (iFt * 12) + iInch;
+                          var tCm = tInch * 2.54;
+                          var tM = tCm / 100;
+
+                          var bmi = iWt / (tM * tM);
+
+                          //condition for changing color and msg for bmi value
+                          var msg = '';
+                          if (bmi > 25) {
+                            msg = "You are Over Weight";
+                            bgColor = Colors.orange.shade200;
+                          } else if (bmi < 18) {
+                            msg = "You are Under Weight";
+                            bgColor = Colors.red.shade200;
+                          } else {
+                            msg = "You are Healthy";
+                            bgColor = Colors.green.shade200;
+                          }
+
+                          setState(() {
+                            result =
+                                ' $msg \n Your BMI is : ${bmi.toStringAsFixed(2)}';
+                          });
+                        } else {
+                          setState(() {
+                            result = "Please Fill all required blanks!!";
+                          });
+                        }
+                      },
+                      child: Text('Calculate')),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    result,
+                    style: TextStyle(fontSize: 28),
+                  ),
+                ],
+              ),
             ),
           ),
         ));
