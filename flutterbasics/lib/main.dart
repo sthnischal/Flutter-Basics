a5b894c7d0b66c541d17e0d5d35598a58a219d87
sthnischal <shresthanischal123@gmail.com>
2023-07-12 13:26:32 +0545

    Animated Opacity Widget

--- flutterbasics/lib/main.dart
+++ flutterbasics/lib/main.dart
@@ -27,68 +27,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _width = 200.0;
-
-  var _height = 100.0;
-
-  bool flag = true;
-
-  //Color bgColor = Colors.blueGrey;
-
-  Decoration myDecor = BoxDecoration(
-      borderRadius: BorderRadius.circular(2), color: Colors.blueGrey);
-
+  var myOpacity = 1.0;
+  bool isVisible = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Animation Example'),
+          title: Text('Animation Opacity Example'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              AnimatedContainer(
-                width: _width,
-                height: _height,
-                // color: bgColor,
-                //curve: Curves.fastOutSlowIn,
-                //curve: Curves.slowMiddle,
-                //curve: Curves.bounceIn,
-                //curve: Curves.bounceInOut,
-                curve: Curves.bounceOut,
-                decoration: myDecor,
+              AnimatedOpacity(
+                opacity: myOpacity,
                 duration: Duration(seconds: 2),
+                //curve: Curves.slowMiddle,
+                //curve: Curves.elasticIn,
+                curve: Curves.bounceIn,
+                child: Container(
+                  width: 200,
+                  height: 100,
+                  color: Colors.blue,
+                ),
               ),
               ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    //for changes toggling
-                    if (flag) {
-                      _width = 100.0;
-                      _height = 200.0;
-                      //  bgColor = Colors.orange;
-
-                      myDecor = BoxDecoration(
-                        borderRadius: BorderRadius.circular(21),
-                        color: Colors.orange,
-                      );
-                      //for flag value change to original
-                      flag = false;
-                    } else {
-                      _width = 200.0;
-                      _height = 100.0;
-                      //bgColor = Colors.blueGrey;
-                      myDecor = BoxDecoration(
-                        borderRadius: BorderRadius.circular(2),
-                        color: Colors.blueGrey,
-                      );
-                      flag = true;
-                    }
-                  });
-                },
-                child: Text('Animate'),
-              )
+                  onPressed: () {
+                    setState(() {
+                      //toggling with button
+                      if (isVisible) {
+                        myOpacity = 0.0; //invisible
+                        isVisible = false;
+                      } else {
+                        myOpacity = 1.0; //visible
+                        isVisible = true;
+                      }
+                    });
+                  },
+                  child: Text('Animate'))
             ],
           ),
         ));
