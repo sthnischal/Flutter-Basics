e278d22cf048a50067e2fffcf2be3bf21fdb398f
sthnischal <shresthanischal123@gmail.com>
2023-07-23 14:29:28 +0545

    To do item with multiple data are added for testing and todo ui is ready.

--- todo_app/lib/screens/home.dart
+++ todo_app/lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
+import 'package:todo_app/widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -9,9 +10,29 @@ class Home extends StatelessWidget {
       appBar: _buildAppbar(),
       // drawer: Drawer(backgroundColor: tdBGColor),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
+        padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
         child: Column(
-          children: [searchBox()],
+          children: [
+            searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                      margin: EdgeInsets.only(top: 50, bottom: 20),
+                      child: Text('All ToDos',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ))),
+                  ToDoItem(),
+                  ToDoItem(),
+                  ToDoItem(),
+                  ToDoItem(),
+                  ToDoItem(),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
