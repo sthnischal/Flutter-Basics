e206aa73deaf35034ca7c99ed54ed7f0f0b22896
sthnischal <shresthanischal123@gmail.com>
2023-07-23 15:58:43 +0545

    App UI completed but bottom button  need to make exact same

--- todo_app/lib/screens/home.dart
+++ todo_app/lib/screens/home.dart
@@ -12,34 +12,84 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppbar(),
       // drawer: Drawer(backgroundColor: tdBGColor),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                      margin: EdgeInsets.only(top: 50, bottom: 20),
-                      child: Text('All ToDos',
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.w500,
-                          ))),
-                  // ToDoItem(),
-                  // ToDoItem(),
-                  // ToDoItem(),
-                  // ToDoItem(),
-                  for (ToDo todo1 in todosList)
-                    ToDoItem(
-                        todo:
-                            todo1), //here todo1 is just variable in loop that take value from our list
-                ],
-              ),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                          margin: EdgeInsets.only(top: 50, bottom: 20),
+                          child: Text('All ToDos',
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.w500,
+                              ))),
+                      // ToDoItem(),
+                      // ToDoItem(),
+                      // ToDoItem(),
+                      // ToDoItem(),
+                      for (ToDo todo1 in todosList)
+                        ToDoItem(
+                            todo:
+                                todo1), //here todo1 is just variable in loop that take value from our list
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Add a new todo item',
+                          border: InputBorder.none)),
+                )),
+                Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                  ),
+                  child: ElevatedButton(
+                    child: Text(
+                      '+',
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    ),
+                    onPressed: () {},
+                    //style for button
+                    style: ElevatedButton.styleFrom(
+                      primary: tdBlue, // color of app
+                      minimumSize: Size(50, 50),
+                      elevation: 10, //background of icon
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
