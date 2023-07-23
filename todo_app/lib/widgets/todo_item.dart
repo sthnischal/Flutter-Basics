2afca805419ee953b07bf5f93355b149378050c0
sthnischal <shresthanischal123@gmail.com>
2023-07-23 15:39:15 +0545

    checkbox value are dynamic based on isDone value true

--- todo_app/lib/widgets/todo_item.dart
+++ todo_app/lib/widgets/todo_item.dart
@@ -19,7 +19,7 @@ class ToDoItem extends StatelessWidget {
           contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
           tileColor: Colors.white,
           leading: Icon(
-            Icons.check_box,
+            todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
             color: tdBlue,
           ),
           title: Text(
@@ -28,7 +28,8 @@ class ToDoItem extends StatelessWidget {
             style: TextStyle(
               fontSize: 16,
               color: tdBlack,
-              decoration: TextDecoration.lineThrough,
+              //decoration: TextDecoration.lineThrough,
+              decoration: todo.isDone ? TextDecoration.lineThrough : null,
             ),
           ),
           trailing: Container(
