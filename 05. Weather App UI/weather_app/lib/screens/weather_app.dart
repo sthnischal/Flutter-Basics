ca47ea8482414a72b53eaec5643ad74ae4205b19
sthnischal <shresthanischal123@gmail.com>
2023-07-26 08:02:08 +0545

    creating slider dots on top of the screen

--- 05. Weather App UI/weather_app/lib/screens/weather_app.dart
+++ 05. Weather App UI/weather_app/lib/screens/weather_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../widgets/single_weather.dart';
 
 class WeatherApp extends StatelessWidget {
   @override
@@ -47,211 +48,48 @@ class WeatherApp extends StatelessWidget {
                     color: Colors.black38), //make image little bit transparent
               ),
               Container(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        //this widget help to locate our text in between the overall screen
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            //top column area
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: 150,
-                                ),
-                                Text(
-                                  'Kathmandu',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 35,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  '7:18 AM Wednesday, 26 July 2023',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                )
-                              ],
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '24\u2103', //unicode value for degree celcius
-                                  style: GoogleFonts.lato(
-                                      fontSize: 85,
-                                      fontWeight: FontWeight.w300,
-                                      color: Colors.white),
-                                ),
-                                Row(
-                                  children: [
-                                    SvgPicture.asset(
-                                      'assets/moon.svg',
-                                      width: 30,
-                                      height: 30,
-                                      color: Colors.white,
-                                    ),
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Text(
-                                      'Night',
-                                      style: GoogleFonts.lato(
-                                          fontSize: 22,
-                                          fontWeight: FontWeight.w500,
-                                          color: Colors.white),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.symmetric(vertical: 40),
-                            decoration: BoxDecoration(
-                                border: Border.all(color: Colors.white30)),
-                          )
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              children: [
-                                Text(
-                                  'Wind',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  '10',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  'Km/hr',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Stack(
-                                  children: [
-                                    Container(
-                                      height: 5,
-                                      width: 50,
-                                      color: Colors.grey,
-                                    ),
-                                    Container(
-                                      height: 5,
-                                      width: 10,
-                                      color: Colors.greenAccent,
-                                    )
-                                  ],
-                                )
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Text(
-                                  'Rain',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  '2',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  '%',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Stack(
-                                  children: [
-                                    Container(
-                                      height: 5,
-                                      width: 50,
-                                      color: Colors.grey,
-                                    ),
-                                    Container(
-                                      height: 5,
-                                      width: 2,
-                                      color: Colors.redAccent,
-                                    )
-                                  ],
-                                )
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Text(
-                                  'Humidity',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  '10',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Text(
-                                  '%',
-                                  style: GoogleFonts.lato(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                Stack(
-                                  children: [
-                                    Container(
-                                      height: 5,
-                                      width: 50,
-                                      color: Colors.grey,
-                                    ),
-                                    Container(
-                                      height: 5,
-                                      width: 10,
-                                      color: Colors.redAccent,
-                                    )
-                                  ],
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ))
+                margin: EdgeInsets.only(
+                  top: 140,
+                  left: 15,
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 5),
+                      width: 12,
+                      height: 5,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 5),
+                      width: 5,
+                      height: 5,
+                      decoration: BoxDecoration(
+                          color: Colors.white54,
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 5),
+                      width: 5,
+                      height: 5,
+                      decoration: BoxDecoration(
+                          color: Colors.white54,
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 5),
+                      width: 5,
+                      height: 5,
+                      decoration: BoxDecoration(
+                          color: Colors.white54,
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                    )
+                  ],
+                ),
+              ),
+              SingleWeather()
             ],
           ),
         ));
