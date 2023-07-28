82404ea1653be83721c7a53a3b25d2f7d4380c7d
sthnischal <shresthanischal123@gmail.com>
2023-07-28 07:54:43 +0545

    bottom navigation bar is completed with working onpressed function on icon

--- 06. News App UI/news_app/lib/screens/home_screen.dart
+++ 06. News App UI/news_app/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/bottom_nav_bar.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -7,6 +9,10 @@ class HomeScreen extends StatelessWidget {
   static const routeName = '/';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: const BottomNavBar(index: 0),
+      body: Container(),
+    );
   }
 }
