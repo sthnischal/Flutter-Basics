efa31eddf0649e4d9edad5a7272f7408f6dd244d
sthnischal <shresthanischal123@gmail.com>
2023-07-30 15:05:06 +0545

    breaking news section is added withut clickable function

--- 06. News App UI/news_app/lib/screens/home_screen.dart
+++ 06. News App UI/news_app/lib/screens/home_screen.dart
@@ -25,9 +25,96 @@ class HomeScreen extends StatelessWidget {
       ),
       bottomNavigationBar: const BottomNavBar(index: 0),
       extendBodyBehindAppBar: true,
-      body: ListView(
-          padding: EdgeInsets.zero,
-          children: [_NewsOfTheDay(article: article)]),
+      body: ListView(padding: EdgeInsets.zero, children: [
+        _NewsOfTheDay(article: article),
+        _BreakingNews(
+            articles:
+                Article.articles), //take list of article from article model
+      ]),
+    );
+  }
+}
+
+class _BreakingNews extends StatelessWidget {
+  const _BreakingNews({
+    super.key,
+    required this.articles,
+  });
+  final List<Article> articles;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Breaking News',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'More',
+                style: Theme.of(context).textTheme.bodyLarge,
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 250,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: articles.length,
+                itemBuilder: ((context, index) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    margin: EdgeInsets.only(right: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ImageContainer(
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          imageUrl: articles[index].imageUrl,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          articles[index].title,
+                          maxLines: 2, //showing max line of text 2 below image
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge!
+                              .copyWith(
+                                  fontWeight: FontWeight.bold, height: 1.5),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          '${DateTime.now().difference(articles[index].createdAt).inHours} Hours ago',
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          '${articles[index].author}',
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                      ],
+                    ),
+                  );
+                })),
+          )
+        ],
+      ),
     );
   }
 }
