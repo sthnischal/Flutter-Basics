5d58c5f0f99c1225d47810bbe601945e7aec1dad
sthnischal <shresthanischal123@gmail.com>
2023-07-30 15:26:19 +0545

    Discover Screen top section is completed

--- 06. News App UI/news_app/lib/screens/discover_screen.dart
+++ 06. News App UI/news_app/lib/screens/discover_screen.dart
@@ -1,11 +1,86 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/bottom_nav_bar.dart';
+
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({Key? key}) : super(key: key);
 
   static const routeName = '/discover';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+          color: Colors.black,
+        ),
+      ),
+      bottomNavigationBar: const BottomNavBar(index: 1),
+      //extendBodyBehindAppBar: true,  // it is used for putting any value from top of the screen
+      body:
+          ListView(padding: EdgeInsets.all(20.0), children: [_DiscoverNews()]),
+    );
+  }
+}
+
+class _DiscoverNews extends StatelessWidget {
+  const _DiscoverNews({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height *
+          0.25, //for top section took 25% of the whole screen
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Discover',
+            style: Theme.of(context).textTheme.headline4!.copyWith(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w900,
+                ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            'News from all over the world',
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: 'Search',
+                fillColor: Colors.grey.shade100,
+                filled: true,
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                suffixIcon: RotatedBox(
+                  //this widget is used to rotate 90 tune icon
+                  quarterTurns: 1,
+                  child: Icon(
+                    Icons.tune,
+                    color: Colors.grey,
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide.none,
+                )),
+          )
+        ],
+      ),
+    );
   }
 }
