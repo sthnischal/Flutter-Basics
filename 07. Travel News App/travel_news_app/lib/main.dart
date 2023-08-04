97f5f039eab77e6d3efccbd5481dc30ea9166df8
sthnischal <shresthanischal123@gmail.com>
2023-08-04 14:54:28 +0545

    setting custom search bar section but error with search icon

--- 07. Travel News App/travel_news_app/lib/main.dart
+++ 07. Travel News App/travel_news_app/lib/main.dart
@@ -75,6 +75,53 @@ class HomeScreen extends StatelessWidget {
               ],
             )
           ],
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(kBorderRadius),
+              color: kWhite,
+              boxShadow: [
+                BoxShadow(
+                  color: kDarkBlue.withOpacity(0.051),
+                  offset: const Offset(0.0, 3.0),
+                  blurRadius: 24.0,
+                  spreadRadius: 0.0,
+                )
+              ]),
+          child: Row(children: [
+            Expanded(
+                child: TextField(
+              style: kPoppinsRegular.copyWith(
+                color: kBlue,
+                fontSize: SizeConfig.blockSizeHorizontal! * 3,
+              ),
+              controller: TextEditingController(),
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 13),
+                  hintText: 'Search for Article ...',
+                  border: kBorder,
+                  errorBorder: kBorder,
+                  disabledBorder: kBorder,
+                  focusedBorder: kBorder,
+                  focusedErrorBorder: kBorder,
+                  hintStyle: kPoppinsRegular.copyWith(
+                    color: kLightGrey,
+                    fontSize: SizeConfig.blockSizeHorizontal! * 3,
+                  )),
+            )),
+            Container(
+              decoration: BoxDecoration(
+                  color: kBlue,
+                  borderRadius: BorderRadius.circular(kBorderRadius)),
+              child: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset('assets/search_icon.svg'),
+              ),
+            )
+          ]),
         )
       ],
     ));
