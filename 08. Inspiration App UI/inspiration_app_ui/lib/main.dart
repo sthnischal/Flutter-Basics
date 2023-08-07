2905187af2889787ddf25e7d812658863785dc6e
sthnischal <shresthanischal123@gmail.com>
2023-08-07 07:29:52 +0545

    top section and search bar is completed

--- 08. Inspiration App UI/inspiration_app_ui/lib/main.dart
+++ 08. Inspiration App UI/inspiration_app_ui/lib/main.dart
@@ -27,7 +27,49 @@ class _HomePageState extends State<HomePage> {
           color: Colors.black87,
         ),
       ),
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(30))),
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Find Your',
+                      style: TextStyle(color: Colors.black, fontSize: 25),
+                    ),
+                    SizedBox(height: 3),
+                    Text(
+                      'Inspiration',
+                      style: TextStyle(color: Colors.black, fontSize: 40),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(15)),
+                      child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              prefixIcon:
+                                  Icon(Icons.search, color: Colors.black87),
+                              hintText: "Search you're looking for...",
+                              hintStyle:
+                                  TextStyle(color: Colors.grey, fontSize: 15))),
+                    )
+                  ]),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
