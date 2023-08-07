2b8dd0e0c41f600874cba98797a53116cf7d94f4
sthnischal <shresthanischal123@gmail.com>
2023-08-07 08:02:28 +0545

    image with gradient and slider is added in bottom section

--- 08. Inspiration App UI/inspiration_app_ui/lib/main.dart
+++ 08. Inspiration App UI/inspiration_app_ui/lib/main.dart
@@ -64,12 +64,56 @@ class _HomePageState extends State<HomePage> {
                               hintText: "Search you're looking for...",
                               hintStyle:
                                   TextStyle(color: Colors.grey, fontSize: 15))),
-                    )
+                    ),
                   ]),
             ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Promo Today',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 15),
+                    Container(
+                      height: 200,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          promoCard(''),
+                        ],
+                      ),
+                    )
+                  ]),
+            )
           ],
         ),
       ),
     );
   }
+}
+
+Widget promoCard(image) {
+  return AspectRatio(
+    aspectRatio: 2 / 3,
+    child: Container(
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+            fit: BoxFit.cover, image: AssetImage('assets/images/one.jpg')),
+      ),
+      child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+                Colors.black.withOpacity(0.8),
+                Colors.black.withOpacity(0.1)
+              ]))),
+    ),
+  );
 }
